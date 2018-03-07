99106e09ab99edf3304efd103379ced9d0a287cb
rutvikhp <rutvikhp@gmail.com>
2018-03-07 16:32:47 -0600

    single card updated

--- client/components/DetailWeather.js
+++ client/components/DetailWeather.js
@@ -17,43 +17,65 @@ class DetailWeather extends Component{
     return (
       <Grid>
         <Grid.Row>
-          <Grid.Column width={2}>
+          <Grid.Column width = {3} style={divStyle}>
+            Time
           </Grid.Column>
-          <Grid.Column width={12}>
+          <Grid.Column width = {1}>
+          </Grid.Column>
+          <Grid.Column width = {2}>
+            Summary
+          </Grid.Column>
+          <Grid.Column width = {2}>
+            Temp.
+          </Grid.Column>
+          <Grid.Column width = {2}>
+            Feels Like
+          </Grid.Column>
+          <Grid.Column width = {2}>
+            Wind Speed
+          </Grid.Column>
+        </Grid.Row>
             {
               this.props.detailWeather.map((data, idx) => (
-                <Grid.Row key = {idx}>
-                  <Grid.Column width ={6}>
+                <Grid.Row key = {idx} >
+                  <Grid.Column width ={3} style={divStyle}>
                     {arr[idx]}
                     {  (idx<12) ?
                       <span> am</span>
                       :
                       <span> pm</span>
                     }
-                  </Grid.Column>
-                  <Grid.Column width={6}>
-
+                    </Grid.Column>
+                  <Grid.Column width ={1} >
                     <ReactAnimatedWeather
                       icon={data.icon}
                       color="orange"
-                      size={16}
+                      size={26}
                       animate={true}
                     />
-
+                  </Grid.Column>
+                  <Grid.Column width = {2}>
+                    {data.summary}
+                  </Grid.Column>
+                  <Grid.Column width = {2}>
+                    {data.temp}
+                  </Grid.Column>
+                  <Grid.Column width = {2}>
+                    {data.feelsLike}
+                  </Grid.Column>
+                  <Grid.Column width = {2}>
+                    {data.windSpeed}
                   </Grid.Column>
                 </Grid.Row>
               ))
             }
-
-          </Grid.Column>
-          <Grid.Column width={2}>
-          </Grid.Column>
-        </Grid.Row>
       </Grid>
     )
   }
 }
-
+var divStyle = {
+  padding : "0 0 0 50px"
+};
 const mapState = state => {
   return {
     currentWeather: state.currentWeather,
